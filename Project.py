1f325f5feab606e808aafe982421a2f8235b1ebf
prozac631 <prozac631@gmail.com>
2014-04-17 18:56:38 +0400

    Init commit for "Project" class

---
+++ Project.py
@@ -0,0 +1,16 @@
+__author__ = 'prozac631'
+
+
+class Project(self):
+
+    def getProjectTargets(self):
+        pass
+
+    def getBuildConfiguration(self):
+        pass
+
+    def getDepencies(self):
+        pass
+
+    def getWorkspaces(self):
+        pass
